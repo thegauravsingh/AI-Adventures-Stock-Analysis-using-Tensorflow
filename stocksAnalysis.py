fa9f11d2bb9d2b07c0a82042aa1df1e041cb44a5
gauravsingh1012 <gauravsinghmail@gmail.com>
2018-08-04 17:51:28 +0530

    Fixes for Duplicate column issue

--- stocksAnalysis.py
+++ stocksAnalysis.py
@@ -50,8 +50,8 @@ class stockTicker():
 
         if ('Adj. Volume' not in stock.columns):
             stock['Adj. Volume'] = stock['Total Trade Quantity']
-        if exchange == 'NSE':
-            stock['y1'] = stock['y']
+        #if exchange == 'NSE':
+        #    stock['y1'] = stock['y']
         stock['y'] = stock['Adj. Close']
         stock['Daily Change'] = stock['Adj. Close'] - stock['Adj. Open']
 
